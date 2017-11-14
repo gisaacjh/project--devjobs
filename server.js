b3e1ff5e8740f377adf2e8dd45c2d7047adc669a
Isaac <gisaacjh@outlook.com>
2017-11-14 16:08:45 -0600

    add routing: API;pageHome

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const fs = require('fs-extra');
 
+const pageRouter = require('./src/routes/pageRouter.js');
+const apiRouter = require('./src/routes/apiRouter.js');
+
 const app = express();
 const PATH = `${__dirname}/src/views/home.html`;
 
-app.use('/', (req, res) => {
-  fs
-    .readFile(PATH, 'utf-8')
-    .then(data => {
-      res.send(data);
+app.use('/', pageRouter);
+app.use('/api/v1', apiRouter);
 
-    })
+
+//Crar una ruta por si hay un error 404.
+app.use((req, res) => {
+  res.send('<header>404. Not Found</header>')
 });
+
+// app.use('/', (req, res) => {
+//   fs
+//     .readFile(PATH, 'utf-8')
+//     .then(data => {
+//       res.send(data);
+//
+//     })
+// });
 
 const PORT = process.env.PORT || 3000;
 
